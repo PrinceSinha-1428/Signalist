63a204db8724a0b93dad4dae83e025394326524a
Prince <princesinha629@gmail.com>
2025-10-04 22:31:21 +0530

    Implement Auth UI

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,9 +1,57 @@
-import React from 'react'
+"use client";
+
+import FooterLink from '@/components/forms/FooterLink';
+import InputField from '@/components/forms/InputField';
+import { Button } from '@/components/ui/button';
+import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const SignIn: React.FC = () => {
+
+  const { register, handleSubmit, control, formState: { errors, isSubmitting } } = useForm<SignUpFormData>({
+      defaultValues: {
+        email: '',
+        password: '',
+      },
+      mode : 'onBlur'
+    });
+  
+    const onSubmit = async (data: SignUpFormData) => {
+      try {
+        
+      } catch (error) {
+        
+      }
+    }
+
+
   return (
     <>
-      <h1>Sign In</h1>
+       <h1 className='form-title'>Log In Your Account</h1>
+       <form onSubmit={handleSubmit(onSubmit)}className='space-y-5' >
+        
+        <InputField
+            name="email"
+            label="Email"
+            placeholder="johndoe@gmail.com"
+            register={register}
+            error={errors.email}
+            validation={{ required: "Email address is required", pattern: /^\w+@\w\.\w+$/, message: "Email is required" }}
+          />
+        <InputField
+            name="password"
+            label="Password"
+            placeholder="********"
+            register={register}
+            type='password'
+            error={errors.password}
+            validation={{ required: "Password is required", minLength: 8 }}
+          />
+        <Button type='submit' disabled={isSubmitting} className='yellow-btn w-full mt-5' >
+          { isSubmitting ? 'Signing In' : 'Log In'}
+        </Button>
+        <FooterLink text='Don&apos;t have an account' href='/sign-up' linkText='Sign Up' />
+       </form>
     </>
   )
 }
---
+++ components/forms/FooterLink.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+import React from 'react'
+
+const FooterLink: React.FC<FooterLinkProps> = ({ text, linkText, href}) => {
+  return (
+    <div className='text-center pt-4'>
+      <p className='text-sm text-gray-500'>
+        {text}{` `}
+        <Link href={href} className='footer-link'>{linkText}</Link>
+      </p>
+    </div>
+  )
+}
+
+export default FooterLink
